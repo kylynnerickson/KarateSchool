ad4c839fa2b5374ac4470c244078c23cd5485c0b
kylynnerickson <125531228+kylynnerickson@users.noreply.github.com>
2023-11-20 13:17:06 -0600

    1

---
+++ WebApplication1/spx.aspx
@@ -0,0 +1,7 @@
+﻿<asp:GridView ID="GridView1" runat="server"></asp:GridView>
+
+<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="spx.aspx.cs" Inherits="WebApplication1.spx" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ WebApplication1/spx.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class spx : System.Web.UI.Page
+    {
+        AdminDataContext dbcon;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connstring = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\ekyly\\OneDrive\\Desktop\\karate\\WebApplication1\\App_Data\\KarateData.mdf;Integrated Security=True;Connect Timeout=30";
+            dbcon= new AdminDataContext(connstring);
+
+            //select all members
+            var result = from item in dbcon.Members
+                         orderby item.LName, item.FName
+                         select item;
+
+            //gridview
+
+            MembersGridView.DataSource = result;
+            MembersGridView.databind;
+        }
+    }
+}
